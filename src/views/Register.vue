bac6ce1cc5d522bb9e957623e5b00cd7c7330cdd
Eugene <evgen1y88@icloud.com>
2021-06-29 19:14:24 +0200

    Joke, registration work:)

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -116,7 +116,7 @@ export default {
       }
       try {
         await this.$store.dispatch('register', formData)
-        this.$router.push("/")
+        this.$router.push('/')
       } catch (e) {}
     }
   }
