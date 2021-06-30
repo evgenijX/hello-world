4abcf662645d1084b9fedf5f9cdb50960e474e64
Eugene <evgen1y88@icloud.com>
2021-06-30 19:21:32 +0200

    Edit categories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,16 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
-        <CategoryCreate @created="addNewCategoty"/>
-        <CategoryEdit />
+      <Loader v-if="loading" />
+      <div class="row" v-else>
+        <CategoryCreate @created="addNewCategoty" />
+        <CategoryEdit
+          v-if="categories.length"
+          :categories="categories"
+          :key="categories.length + updateCounter"
+          @updated="updateCategories"
+        />
+        <p v-else class="center">Категорий пока нет</p>
       </div>
     </section>
   </div>
@@ -16,19 +23,30 @@
 import CategoryCreate from "@/components/CategoryCreate";
 import CategoryEdit from "@/components/CategoryEdit";
 export default {
+  name: "categories",
+  data: () => ({
+    categories: [],
+    loading: true,
+    updateCounter: 0,
+  }),
+  async mounted() {
+    this.categories = await this.$store.dispatch("fetchCategories");
+    this.loading = false;
+  },
   components: {
-    name: "categories",
-    data: () => ({
-      categories: []
-    }),
     CategoryCreate,
     CategoryEdit,
   },
   methods: {
-    addNewCategoty(category){
-      this.categories.push(category)
-      console.log(this.categories)
-    }
-  }
+    addNewCategoty(category) {
+      this.categories.push(category);
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex((c) => c.id === category.id);
+      this.categories[idx].title = category.title;
+      this.categories[idx].limit = category.limit;
+      this.updateCounter++;
+    },
+  },
 };
 </script>
